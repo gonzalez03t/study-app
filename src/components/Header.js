7f46ddd1c3bca88be2c9b037ba87820ae3df6c1c
Jesus Gonzalez <gonzalez03t@gmail.com>
2022-02-13 22:48:22 -0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { StyledHeader } from "./styles/Header.styled"
+
+export default function Header() {
+    return (
+        <StyledHeader bg={"red"}>
+            <h1>Hubble</h1>
+        </StyledHeader>
+    )
+}
